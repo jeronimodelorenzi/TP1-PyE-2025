6f3480e5b9acce20d252d4cd4fe6b0f7a37e9ae4
ignaciorimini <ignaciorimini9@gmail.com>
2025-04-23 18:56:50 -0300

    Agrego analisis bivariado

---
+++ analisis/02_analisis_cant_integrantes_y_cant_dormitorios.R
@@ -0,0 +1,20 @@
+# _________________
+# TIPO_TENENCIA y CANT_DORMITORIOS: Variable categórica nominal y cuantitativa discreta.
+# Visualización, estadísticas y gráficos.
+library(tidyverse)
+attach(datos_limpios)
+
+### Coeficiente de correlación de Pearson.
+cor(datos_limpios$cant_integrantes, datos_limpios$cant_dormitorios, method = "pearson")
+
+### Gráfico de dispersión básico
+datos_limpios %>% 
+  ggplot(aes(x=cant_integrantes, y=cant_dormitorios)) +
+  geom_jitter(width=0.2, height=0.2, alpha=0.6, color="#4682B4", size=2) +
+  scale_x_continuous(breaks=0:max(datos_limpios$cant_integrantes)) +
+  scale_y_continuous(breaks=0:max(datos_limpios$menores_18)) +
+  labs(x="Cantidad Integrantes", 
+       y="Cantidad Dormitorios", 
+       title = "Relación entre cantidad de integrantes y cantidad de dormitorios")
+
+
